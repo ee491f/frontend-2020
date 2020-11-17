caf5a167f0dabaabcfbd0e73ac1d7b9a5d05673d
Zhao Li <zhaol@hawaii.edu>
2020-11-17 11:01:33 -1000

    buttons working

--- files/example.js
+++ files/example.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    $( "[function=hide]" ).on( "click", function() {
+        $('a').fadeOut();
+    });
+    $( "[function=show]" ).on( "click", function() {
+        $('a').fadeIn();
+    });
 });
