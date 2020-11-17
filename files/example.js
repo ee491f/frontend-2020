92540acfbfe7326072f6d25565607bc8c858f6e9
Zhao Li <zhaol@hawaii.edu>
2020-11-17 11:08:24 -1000

    revert back to desired behavior

--- files/example.js
+++ files/example.js
@@ -1,9 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $( "[function=hide]" ).on( "click", function() {
-        $('a').remove();
-        $('br').remove();
-        $('.outer, .inner').removeClass(['outer', 'inner'])
+        $('a').fadeOut();
     });
     $( "[function=show]" ).on( "click", function() {
         $('a').fadeIn();
