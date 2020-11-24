5a2c68b26caf64448c3d256999f251e4ed4279f0
Zhao Li <zhaol@hawaii.edu>
2020-11-24 12:19:47 -1000

    refactor to function

--- files/example-jquery.js
+++ files/example-jquery.js
@@ -1,3 +1,27 @@
+function settingColors(event) {
+  $("[function=background-switcher] .warning").fadeOut();
+  let colors = {
+    badColors: [
+      'black',
+      'white',
+    ],
+    goodColors: [
+      'blue',
+      'yellow',
+    ]
+  };
+  let color = $("[function=background-switcher] [type=text][name=color]").val();
+  if (colors['badColors'].includes(color)) {
+    $("[function=background-switcher] .warning.bad-color").fadeIn();
+  } else if (colors['goodColors'].includes(color)) {
+    $('[function=background-switcher]').css("background-color", color);
+  } else {
+    $("[function=background-switcher] .warning.unknown-color").fadeIn();
+  }
+};
+
+
+
 $( document ).ready(function() {
   console.log( "jquery is ready" );
   $( "[function=hide]" ).on( "click", function() {
@@ -8,24 +32,6 @@ $( document ).ready(function() {
   });
 
   $('[function=background-switcher] [type=button]').on("click", function(event) {
-    let colors = {
-      badColors: [
-        'black',
-        'white',
-      ],
-      goodColors: [
-        'blue',
-        'yellow',
-      ]
-    };
-    let color = $("[function=background-switcher] [type=text][name=color]").val();
-    if (colors['badColors'].includes(color)) {
-      $("[function=background-switcher] .warning.bad-color").fadeIn();
-    } else if (colors['goodColors'].includes(color)) {
-      $("[function=background-switcher] .warning").fadeOut();
-      $('[function=background-switcher]').css("background-color", color);
-    } else {
-      $("[function=background-switcher] .warning.unknown-color").fadeIn();
-    }
+    settingColors(event);
   });
 });
