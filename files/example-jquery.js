08cb7556b66ded6632bf661558327bbf0051a873
Zhao Li <zhaol@hawaii.edu>
2020-11-24 12:22:15 -1000

    refactor function into separate file

--- files/example-jquery.js
+++ files/example-jquery.js
@@ -1,27 +1,3 @@
-function settingColors(event) {
-  $("[function=background-switcher] .warning").fadeOut();
-  let colors = {
-    badColors: [
-      'black',
-      'white',
-    ],
-    goodColors: [
-      'blue',
-      'yellow',
-    ]
-  };
-  let color = $("[function=background-switcher] [type=text][name=color]").val();
-  if (colors['badColors'].includes(color)) {
-    $("[function=background-switcher] .warning.bad-color").fadeIn();
-  } else if (colors['goodColors'].includes(color)) {
-    $('[function=background-switcher]').css("background-color", color);
-  } else {
-    $("[function=background-switcher] .warning.unknown-color").fadeIn();
-  }
-};
-
-
-
 $( document ).ready(function() {
   console.log( "jquery is ready" );
   $( "[function=hide]" ).on( "click", function() {
